e001faa010cdd2102dbae6bd8d01d6e59f6f3251
Mathioussee <58639269+Mathiousse@users.noreply.github.com>
2024-01-19 16:18:16 +0100

    sudo fuser -k 80/tcp

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # 'stole' the idea from Célian
 echo "Deploying to prod environment..."
+sudo fuser -k 80/tcp
 sudo docker compose -f docker-compose.prod.yml down
 sudo docker compose -f docker-compose.prod.yml up -d
