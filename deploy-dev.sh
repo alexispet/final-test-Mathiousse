3d8056742f827f88ca9db9a715e3685d0bba9c5e
Mathioussee <58639269+Mathiousse@users.noreply.github.com>
2024-01-19 15:57:24 +0100

    no - in sh

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # 'stole' the idea from Célian
 echo "Deploying to dev environment..."
-cd /path/to/your/project
 sudo docker compose -f docker-compose.dev.yml down
-sudo docker-compose -f docker-compose.dev.yml up -d
+sudo docker compose -f docker-compose.dev.yml up -d
--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # 'stole' the idea from Célian
 echo "Deploying to prod environment..."
-cd /path/to/your/project
 sudo docker compose -f docker-compose.prod.yml down
-sudo docker-compose -f docker-compose.prod.yml up -d
+sudo docker compose -f docker-compose.prod.yml up -d
