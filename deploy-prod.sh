b64dc0e37be7f1a7a0a111873fa615d19650db24
Mathioussee <58639269+Mathiousse@users.noreply.github.com>
2024-01-19 16:50:30 +0100

    dont kill the 80 anymore

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # 'stole' the idea from Célian
 echo "Deploying to prod environment..."
-sudo fuser -k 80/tcp
 sudo docker compose -f docker-compose.prod.yml down
 sudo docker compose -f docker-compose.prod.yml up -d
